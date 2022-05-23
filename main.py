f47d66d98dd2b488378892a6e8d9eac62a010ce3
vanetlabiitj <106070754+vanetlabiitj@users.noreply.github.com>
2022-05-23 22:52:19 +0530

    Add files via upload

---
+++ main.py
@@ -0,0 +1,40 @@
+import torch
+from torch.utils.data import DataLoader
+from torch.utils.data import TensorDataset
+import torch.nn as nn
+import torch.nn.functional as F
+import torch.optim as optim
+from utils import *
+from model import *
+from config import *
+
+
+
+device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+print(device)
+
+#load dataset
+X_train, X_test, y_train, y_test = load_full_dataset()
+print('Training dataset shape: ', X_train.shape, y_train.shape)
+print('Testing dataset shape: ', X_test.shape, y_test.shape)
+params = Params(X_train)
+
+#converting data into tensor form and creating dataloader
+X_train = torch.Tensor(X_train).to(device)
+X_test = torch.Tensor(X_test).to(device)
+y_train = torch.Tensor(y_train).reshape(-1).to(device)
+y_test = torch.Tensor(y_test).reshape(-1).to(device)
+
+train_dataset = TensorDataset(X_train,  y_train)
+test_dataset = TensorDataset(X_test,  y_test)
+
+train_dataloader = DataLoader(dataset=train_dataset, batch_size=params.batch_size, shuffle=True)
+test_dataloader = DataLoader(dataset=test_dataset, batch_size=params.batch_size, shuffle=False)
+
+lossfn = nn.CrossEntropyLoss()
+
+model = Net(params.n_channels,params.time_steps, params.ff_dim,params.n_head,params.n_classes,params.n_layers,params.dropout, device)
+model.to(device)
+optimizer= optim.Adam(model.parameters(), lr= params.learning_rate)
+
+train(model,params.n_epochs,train_dataloader,optimizer,lossfn,test_dataloader)
---
+++ utils.py
@@ -0,0 +1,97 @@
+import pickle
+import os
+from sklearn.model_selection import train_test_split
+import numpy as np
+import torch
+
+from sklearn.metrics import confusion_matrix
+from sklearn.metrics import classification_report
+from sklearn.metrics import accuracy_score
+
+#read data from pkl file
+def load_full_dataset(data_path='TransDBC/data'):
+
+    with open(os.path.join(data_path, 'motorway_dataset.pkl'), 'rb') as f:
+        save = pickle.load(f,encoding='bytes')
+        motorway_dataset = save[b'dataset']
+        motorway_labels = save[b'labels']
+        del save
+
+    with open(os.path.join(data_path,'secondary_dataset.pkl'), 'rb') as f:
+        save = pickle.load(f,encoding='bytes')
+        secondary_dataset = save[b'dataset']
+        secondary_labels = save[b'labels']
+        del save
+
+    dataset = np.concatenate((motorway_dataset,secondary_dataset), axis=0)
+    labels = np.concatenate((motorway_labels,secondary_labels), axis=0)
+
+    X_train, X_test, y_train, y_test = train_test_split(dataset, labels, test_size=0.20, random_state=42)
+
+    return X_train, X_test, y_train, y_test
+
+#test function
+def test(model,testloader,lossfn,onehotencoding=True,n_class=3):
+  predlist=torch.zeros(0,dtype=torch.long, device='cpu')
+  lbllist=torch.zeros(0,dtype=torch.long, device='cpu')
+  running_loss = 0.0
+  for i, data in enumerate(testloader, 0):
+    # get the inputs; data is a list of [inputs, labels]
+    inputs, labels = data 
+    #inputs = inputs.view(-1,1,64,9)  
+    outputs = model(inputs)
+    if onehotencoding == True:
+      new_labels = torch.nn.functional.one_hot(labels.to(torch.int64),n_class).to(torch.float32)
+    else:
+      new_labels = labels
+    loss = lossfn(outputs, new_labels)
+    # print statistics
+    running_loss += loss.item()
+    predlist=torch.cat([predlist,torch.argmax(outputs, dim=1).view(-1).cpu()])
+    lbllist=torch.cat([lbllist,labels.view(-1).cpu()])
+  print()
+  print("The test is: ",loss)
+  conf_mat=confusion_matrix(lbllist.numpy(), predlist.numpy())
+  print("The test confusion matrix is: ",conf_mat)
+  class_accuracy=100*conf_mat.diagonal()/conf_mat.sum(1)
+  print("the test accuracy acheived is: ")
+  print(np.sum(class_accuracy)/n_class)
+  print("classification report:")
+  print(classification_report(lbllist.numpy(), predlist.numpy()))
+  print()
+
+#model training function
+def train(model,n_epochs,trainloader,optimizer,lossfn,test_dataloader,onehotencoding=True,n_class=3):
+  predlist=torch.zeros(0,dtype=torch.long, device='cpu')
+  lbllist=torch.zeros(0,dtype=torch.long, device='cpu')
+  for epoch in range(n_epochs):  # loop over the dataset multiple times
+    running_loss = 0.0
+    for i, data in enumerate(trainloader, 0):
+      # get the inputs; data is a list of [inputs, labels]
+      inputs, labels = data   
+      # zero the parameter gradients
+      optimizer.zero_grad()
+      # forward + backward + optimize
+      #inputs = inputs.view(-1,1,64,9)
+      outputs = model(inputs)
+      #print(outputs.shape)
+      if onehotencoding == True:
+        #new_labels = torch.tensor(onehotvector[labels])
+        #print(labels.shape)
+        new_labels = torch.nn.functional.one_hot(labels.to(torch.int64),n_class).to(torch.float32)
+      else:
+        new_labels = labels
+      loss = lossfn(outputs, new_labels)
+
+      loss.backward()
+      optimizer.step()
+      # print statistics
+      running_loss += loss.item()
+      predlist=torch.cat([predlist,torch.argmax(outputs, dim=1).view(-1).cpu()])
+      lbllist=torch.cat([lbllist,labels.view(-1).cpu()])
+    print()
+    print("The training loss for epoch ",epoch+1," is: ",loss)
+    # Confusion matrix
+    
+    print("the training accuracy acheived is: ",accuracy_score(lbllist,predlist))
+    test(model,test_dataloader,lossfn)
